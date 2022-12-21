c05a03e22992ad4b3c63aad5a75955a882ed5d58
TeodorTodorovT <teodortodorov752@gmail.com>
2022-12-21 21:21:40 +0200

    Created the search component

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '30dbcfee12mshd865e9981dc3c91p170648jsn14f01d7bd8d5',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
